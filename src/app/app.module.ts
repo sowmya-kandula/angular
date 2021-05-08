46fac53f956ce7a7f391791aafe033123d9b60b6
sowmya <sowmya2431@gmail.com>
2021-05-08 12:25:55 +0530

    app

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { User1Component } from './user1/user1.component';
+import { User2Component } from './user2/user2.component';
+import { User3Component } from './user3/user3.component';
+import { User4Component } from './user4/user4.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    User1Component,
+    User2Component,
+    User3Component,
+    User4Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/user4/user4.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { User4Component } from './user4.component';
+
+describe('User4Component', () => {
+  let component: User4Component;
+  let fixture: ComponentFixture<User4Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ User4Component ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(User4Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
